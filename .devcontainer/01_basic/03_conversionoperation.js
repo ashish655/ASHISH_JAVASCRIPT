0444d1d97a72f8b63528f2db0ba0c835869d1335
Ashish Kumar Thakur <tashishkumar09@gmail.com>
2025-01-18 03:21:38 +0530

    data types operation

--- .devcontainer/01_basic/03_conversionoperation.js
+++ .devcontainer/01_basic/03_conversionoperation.js
@@ -15,15 +15,65 @@
 //let isLoggedIn = "Ashish" => true
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
-console.log(typeof booleanIsLoggedIn);
+ //console.log(booleanIsLoggedIn);
+//console.log(typeof booleanIsLoggedIn);
 
 
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+
+// ********************* Operations ***************
+
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+*/
+//console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log(true);
+//console.log(+true);
+// console.log(+"");
+
+let gameCounter = 1000
+++gameCounter; // => 1001
+gameCounter++  // => 1001
+console.log(gameCounter);
+//gameCounter++;
+//console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
