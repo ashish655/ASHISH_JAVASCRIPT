caebe93df9faa6bfa7c5f7c98cdfe831f6cec6d2
Ashish Kumar Thakur <tashishkumar09@gmail.com>
2025-01-18 01:38:18 +0530

    container config done

---
+++ .devcontainer/01_basic/01variable.js
@@ -0,0 +1,12 @@
+const accountId = 144567
+let accountEmail = "ashish123@gmail.com"
+var accountPassword = "12345"
+accountCity = " Greater Noida"
+
+//accountId = 2
+accountEmail = "ashish212121@gmail.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity])
+
