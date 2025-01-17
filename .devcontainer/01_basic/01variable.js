9f376458f1c892bba416624137edaf4e25e109a1
Ashish Kumar Thakur <tashishkumar09@gmail.com>
2025-01-18 01:51:16 +0530

    container config done

--- .devcontainer/01_basic/01variable.js
+++ .devcontainer/01_basic/01variable.js
@@ -3,10 +3,16 @@ let accountEmail = "ashish123@gmail.com"
 var accountPassword = "12345"
 accountCity = " Greater Noida"
 
-//accountId = 2
+//accountId = 2 // not allowed 
 accountEmail = "ashish212121@gmail.com"
 accountPassword = "21212121"
 accountCity = "Bengaluru"
+let accountstate;
 console.log(accountId);
-console.table([accountEmail,accountId,accountPassword,accountCity])
+
+
+/* prefer not to use var 
+because of issue with bin block space and functinal scope
+*/
+console.table([accountEmail,accountId,accountPassword,accountCity,accountstate])
 
