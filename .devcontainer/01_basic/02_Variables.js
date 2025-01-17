c6fa04f02b92dfa2b5a32a4aa2da53c0f8902395
Ashish Kumar Thakur <tashishkumar09@gmail.com>
2025-01-18 02:22:23 +0530

    datatypes

---
+++ .devcontainer/01_basic/02_Variables.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+//alert(3+3) // we are using nodejs not browser
+console.log(3+3)
+let name = "Ashish"
+let age = 22
+let isLoggedIn = true
+let state = null // null
+let country // undefined
+
+
+// primitive data type
+// number => 2 to power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+
+
+// object
+
+
+console.log(typeof "Ashish" )
+console.log(typeof null); // object
+console.log(typeof undefined);
+
