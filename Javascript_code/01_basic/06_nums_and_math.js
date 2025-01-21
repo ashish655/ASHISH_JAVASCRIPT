84a54ed9b9a05b46ee8702077aa9a975a0f0e90f
Ashish Kumar Thakur <tashishkumar09@gmail.com>
2025-01-22 00:46:51 +0530

    maths and num done

--- Javascript_code/01_basic/06_nums_and_math.js
+++ Javascript_code/01_basic/06_nums_and_math.js
@@ -7,6 +7,30 @@ console.log(balance);
 console.log(balance.toString().length)
 
 console.log(balance.toFixed(2));
-console.log("Maths and num operation done");
+//console.log("Maths and num operation done");
+
+const otherName = 123.8966
+console.log(otherName.toPrecision(3));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+// +++++++++++++++++++++ Maths ++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.floor(4.9));
+
+
+
+
+
+
+
 
 
