50aadd2f371c17ecb83dbeee97bb729f6f93f6d7
Ashish Kumar Thakur <tashishkumar09@gmail.com>
2025-01-29 22:50:02 +0530

    function done

--- Javascript_code/03_basics/01_functions.js
+++ Javascript_code/03_basics/01_functions.js
@@ -1,1 +1,61 @@
-console.log("This is function file");
+
+
+function sayMyName()
+{
+    console.log("A");
+    console.log("S");
+    console.log("H");
+    console.log("I");
+    console.log("S");
+    console.log("H");
+}
+// sayMyName()
+
+
+// function addTwoNumbers(number1,number2) // parameter number1 & number2
+// {
+    
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2) // parameter number1 & number2
+{
+    
+    // let result =number1 + number2
+    // return result
+    return number1 + number2
+}
+const result = addTwoNumbers(5, 5) // argument 5,2
+
+// console.log("Result: ", result);
+
+
+
+function loginUserMessage(username)
+{
+    // if (username === undefined) 
+    if (!username) 
+    {
+        console.log("Please enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("Ashish"))
+
+function calculateCartPrice(val1, val2, ...num1)
+{
+    return num1
+}
+//console.log(calculateCartPrice(200, 300, 400, 500));
+
+const user = {
+    username: "Ashish",
+    price: 1999
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+handleObject(user)
