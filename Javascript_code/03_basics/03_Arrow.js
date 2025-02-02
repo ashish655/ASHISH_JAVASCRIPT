316a15f531c2b963c680adfa69da62cbdbd5806d
Ashish Kumar Thakur <tashishkumar09@gmail.com>
2025-02-02 17:57:18 +0530

    Arrow function complete

--- Javascript_code/03_basics/03_Arrow.js
+++ Javascript_code/03_basics/03_Arrow.js
@@ -4,11 +4,47 @@ const user = {
 
     welcomeMessage: function(){
         console.log(`${this.username}, ${this.price} , welcome to website`);
+        console.log(this);
         
     }
 }
 
-user.welcomeMessage()
-user.username = "sam"
-user.welcomeMessage()
-user.price = 9999
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+// user.price = 9999
+//console.log(this);
+
+// function chai()
+// {
+//     let username = "Ashish"
+//     console.log(this.username);
+    
+// }
+
+// chai()
+
+const chai = () =>
+{
+    let username = "Ashish"
+    console.log(this.username);
+    
+}
+
+//chai()
+
+
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+
+
+// implicit 
+
+
+// const addTwo = (num1, num2) =>   num1 + num2
+const addTwo = (num1, num2) =>  (num1 + num2)
+
+console.log(addTwo(3, 4))
