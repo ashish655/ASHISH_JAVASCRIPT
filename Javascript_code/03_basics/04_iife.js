4f021d687701e7b3f6401ae0b21ed857c682329b
Ashish Kumar Thakur <tashishkumar09@gmail.com>
2025-02-02 18:32:36 +0530

    IIFE Done

---
+++ Javascript_code/03_basics/04_iife.js
@@ -0,0 +1,12 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+
+(function chai(){
+    console.log(`DATABASE CONNECTED`);
+    
+})();
+
+( () => {
+    console.log(`DATABASE CONNECTED TWO`);
+    
+})()
