a86113eb7d14522296a8b041c447472c650ef677
Ashish Kumar Thakur <tashishkumar09@gmail.com>
2025-02-12 02:12:27 +0530

    object in for in loop

---
+++ Javascript_code/05_loops/four.js
@@ -0,0 +1,30 @@
+const myObject = {
+    js: 'JavaScript',
+    cpp: 'C++',
+    rb: 'Ruby',
+    swift: 'Swift by Apple'
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is  for ${myObject[key]}`);
+    
+}
+
+const programming = ["js", "cpp", "rb", "swift", "py", "java"]
+
+for (const key in programming) {
+    //console.log(`Programming language is ${programming[key]}`);
+    
+}
+
+// Maps
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+
+// for (const key in map) {
+//     console.log(key);
+    
+// }
