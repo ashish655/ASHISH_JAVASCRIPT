29847a4b9e6c322cbe5e6715251e1a6044a5d88d
Ashish Kumar Thakur <tashishkumar09@gmail.com>
2025-02-12 01:40:05 +0530

    while and do while loop done

---
+++ Javascript_code/05_loops/two.js
@@ -0,0 +1,25 @@
+
+let index = 0
+while (index <= 10) {
+    
+    // console.log(`value of index is ${index}`);
+    index = index + 2
+
+}
+
+let myArray = ['Flash', 'Arrow', 'Superman', 'Batman', ]
+
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`value  is ${myArray[arr]}`);
+    arr++
+}
+
+// do while loop
+
+let score = 1
+
+do {
+    console.log(`value of score is ${score}`);
+    score++
+} while (score <= 10);
