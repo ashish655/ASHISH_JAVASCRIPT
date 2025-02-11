1ce80590f212d51c07e356e9095c09b7972961e8
Ashish Kumar Thakur <tashishkumar09@gmail.com>
2025-02-11 18:33:45 +0530

    Switch done

--- Javascript_code/Control_flow/switch.js
+++ Javascript_code/Control_flow/switch.js
@@ -8,23 +8,50 @@
 //         break;
 // }
 
-const month = 3
+// const month = 3
+
+
+// switch (month) {
+//     case 1:
+//         console.log("January");
+        
+//         break;
+//     case 2:
+//         console.log("February");
+        
+//         break;
+//     case 3:
+//         console.log("March");
+        
+//         break;
+//     case 4:
+//         console.log("April");
+        
+//         break;
+
+//     default:
+//         console.log("default case match");
+        
+//         break;
+// }
+
+const month = "March"
 
 
 switch (month) {
-    case 1:
+    case "January":
         console.log("January");
         
         break;
-    case 2:
+    case "February":
         console.log("February");
         
         break;
-    case 3:
+    case "March":
         console.log("March");
         
         break;
-    case 4:
+    case "April":
         console.log("April");
         
         break;
